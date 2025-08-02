f5534ef2f6b62a0ca5a7a7381365f26d329636b7
avinash <avinash.kamath@scalekit.com>
2025-08-02 15:33:28 +0530

    Added workflow to publish to personal pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="scalekit-sdk-python",
+    name="a-beta-scalekit",
     version="2.3.0",
     packages=find_packages(),
     install_requires=[
