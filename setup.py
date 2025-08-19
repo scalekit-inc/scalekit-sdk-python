cec44027d86b9e99c240cb98986c1afd892b1c6e
avinash <avinash.kamath@scalekit.com>
2025-08-19 19:59:00 +0530

    add langchain structured tools

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="a-beta-scalekit",
-    version="3.0.1",
+    version="4.0.0",
     packages=find_packages(),
     install_requires=[
         "grpcio>=1.64.1",
