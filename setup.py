ba6648df5c9f5150138e0080fc8a421663df4746
dhaneshbs <dhanesh@scalekit.com>
2025-08-20 18:42:53 +0530

    Add resend_invite method and update proto files (#77)
    * Add resend_invite method and update proto files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="scalekit-sdk-python",
-    version="2.3.2",
+    version="2.3.3",
     packages=find_packages(),
     install_requires=[
         "grpcio>=1.64.1",
