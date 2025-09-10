891910f8f22ebbbe7f31dfbc573e0fe3864ba705
dhaneshbs <dhanesh@scalekit.com>
2025-09-10 15:45:45 +0530

    Update __init__.py

--- scalekit/__init__.py
+++ scalekit/__init__.py
@@ -1,6 +1,5 @@
 
 from scalekit.client import ScalekitClient
 from scalekit.common.scalekit import CodeAuthenticationOptions, AuthorizationUrlOptions
-from scalekit.permissions import PermissionClient
 
 __all__ = ['ScalekitClient', 'AuthorizationUrlOptions', 'CodeAuthenticationOptions']
